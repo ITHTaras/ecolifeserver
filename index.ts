e19342872c09b27811fcc5cdaf1009ded64b4ba0
unknown <tarasgorniko@gmail.com>
2023-01-28 13:55:59 +0200

    deploy error

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 import express from "express";
+import dotenv from "dotenv";
 import bodyParser from "body-parser";
 const app = express();
+dotenv.config();
+
+const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
@@ -13,6 +17,6 @@ app.use("/categories", require("./routes/category"));
 app.use("/users", require("./routes/user"));
 app.use("/points", require("./routes/point"));
 
-app.listen(5000, () => {
-  console.log("Running on 5000");
+app.listen(port, () => {
+  console.log(`Running on ${port}`);
 });
